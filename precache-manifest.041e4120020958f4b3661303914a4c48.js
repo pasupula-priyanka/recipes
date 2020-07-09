45bd77c15dd0c9e8c274a3b6a67e4f4d055b0844
pasupula priyanka <pasupulapriyanka123@gmail.com>
2020-07-09 15:48:11 +0530

    Updates

---
+++ precache-manifest.041e4120020958f4b3661303914a4c48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f1b61c0dd419d18f1dee44f188edd5",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "c48e4e1610e175ec3c91",
+    "url": "/recipes/static/css/main.0a15f66d.chunk.css"
+  },
+  {
+    "revision": "9da3e15a9e66364e59f8",
+    "url": "/recipes/static/js/2.1f9c9539.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes/static/js/2.1f9c9539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c48e4e1610e175ec3c91",
+    "url": "/recipes/static/js/main.95f9c889.chunk.js"
+  },
+  {
+    "revision": "72f5bf5f9650288e41e4",
+    "url": "/recipes/static/js/runtime-main.05ef709f.js"
+  }
+]);
